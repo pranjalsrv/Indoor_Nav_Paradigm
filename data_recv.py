4d7d2dd68d5f9cd6ebdef48e9b5c9673d3e18f7e
Pranjal Srivastava <pranjalsrv@gmail.com>
2019-10-21 11:57:17 +0530

    adj mat done

--- data_recv.py
+++ data_recv.py
@@ -1,12 +1,22 @@
 import pyrebase
 from compute import computing
+import json
 
+testing_config = json.load(open('testing_firebase_config.json', 'r'))
 config = {
-    "apiKey": "AIzaSyA0mYpn-QMrga5sbR44rrZH4wR7Ym3Xiow",
-    "authDomain": "limes-app.firebaseapp.com",
-    "databaseURL": "https://limes-app.firebaseio.com",
-    "storageBucket": "limes-app.appspot.com"
+    "apiKey": testing_config['apiKey'],
+    "authDomain": testing_config['authDomain'],
+    "databaseURL": testing_config['databaseURL'],
+    "storageBucket": testing_config['storageBucket']
 }
+
+
+# config = {
+#     "apiKey": "AIzaSyA0mYpn-QMrga5sbR44rrZH4wR7Ym3Xiow",
+#     "authDomain": "limes-app.firebaseapp.com",
+#     "databaseURL": "https://limes-app.firebaseio.com",
+#     "storageBucket": "limes-app.appspot.com"
+# }
 
 firebase = pyrebase.initialize_app(config)
 
